5817771c988e5bfce6911ada878f36a98dfdcad9
illumaware <66912298+illumaware@users.noreply.github.com>
2024-03-09 23:58:27 +0300

    Update pc.lua

--- pc.lua
+++ pc.lua
@@ -1,5 +1,7 @@
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/illumaware/c/main/pc.lua"))()
+
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Chubsware v2", HidePremium = true, SaveConfig = true, ConfigFolder = "Orion"})
+local Window = OrionLib:MakeWindow({Name = "[Pet Catchers] AIO", HidePremium = true, SaveConfig = false, ConfigFolder = "Orion"})
 
 local player = Window:MakeTab({
 	Name = "Player",
@@ -12,6 +14,7 @@ local main = player:AddSection({
 local elixrs = player:AddSection({
 	Name = "Elixirs"
 })
+
 local auto = Window:MakeTab({
 	Name = "Automation",
 	Icon = "rbxassetid://4483345998",
@@ -29,6 +32,7 @@ local fishing = auto:AddSection({
 local quest = auto:AddSection({
 	Name = "Quests"
 })
+
 local misc = Window:MakeTab({
 	Name = "Misc",
 	Icon = "rbxassetid://4483345998",
@@ -36,12 +40,13 @@ local misc = Window:MakeTab({
 })
 
 local rstorage = game:GetService("ReplicatedStorage").Shared.Framework.Network.Remote
-local codes = {"Release", "Cherry", "lucky", "brite", "ilovefishing", "russoplays", "gravypet"}
+local codes = {"runes", "lucky", "cherry", "cherries", "gravypet", "update1", "russoplays", "release", "void", "ilovefishing", "brite"}
 local shrines = {"egg", "gem", "cube", "berry", "radioactive", "better-cube", "cherry", "ticket", "rune"}
 local vu = game:service'VirtualUser'
 local slp = game:GetService("Players").LocalPlayer
 local sui = slp.PlayerGui.ScreenGui
 local lp = game.Players.LocalPlayer.Character.Humanoid
+local quests = sui.Quests.List:GetChildren()
 
 game:service'Players'.LocalPlayer.Idled:connect(function()
     vu:CaptureController()
@@ -50,34 +55,39 @@ end)
 
 
 -- MAIN
-main:AddToggle({
+main:AddToggle({  -- Godmode
 	Name = "🛡️ Godmode",
 	Default = false,
 	Callback = function(Value)
         godmode = Value
-        if godmode == true then
+        if godmode then
             lp.MaxHealth = math.huge
             lp.Health = math.huge
             warn("[Debug] ✅ Enabled Godmode")
         else
             lp.MaxHealth = 800
             lp.Health = 800
-            warn("[Debug] ❌ Disabled Godmode")
-        end
+        end        
     end
 })
-main:AddDropdown({
+main:AddDropdown({  -- Teleport
 	Name = "🚀 Teleport",
 	Default = "",
-	Options = {"Magma Basin", "Gloomy Grotto", "Dusty Dunes", "Sunset Shores", "Frosty Peaks", "Auburn Woods", "Mellow Meadows", "Pet Park"},
+	Options = {"The Blackmarket", "The Summit", "Magma Basin", "Gloomy Grotto", "Dusty Dunes", "Sunset Shores", "Frosty Peaks", "Auburn Woods", "Mellow Meadows", "Pet Park"},
 	Callback = function(Value)
-		rstorage.Event:FireServer("TeleportBeacon", Value, "Spawn")
-        warn("[Debug] ✅ Teleported to " .. Value)
+        if Value ~= "The Blackmarket" and Value ~= "The Summit" then
+		    rstorage.Event:FireServer("TeleportBeacon", Value, "Spawn")
+            warn("[Debug] ✅ Teleported to " .. Value)
+        else
+            rstorage.Event:FireServer("TeleportBeacon", "Magma Basin", Value)
+            warn("[Debug] ✅ Teleported to " .. Value)
+        end
 	end
 })
 
+
 -- ELIXIRS
-elixrs:AddToggle({
+elixrs:AddToggle({  -- Auto-Use Elixir
 	Name = "🧪 Auto-Use Elixir",
 	Default = false,
 	Callback = function(Value)
@@ -85,7 +95,7 @@ elixrs:AddToggle({
         if AUElixir == true then warn("[Debug] ✅ Enabled Auto-Use Elixir") end    
 	end    
 })
-elixrs:AddDropdown({
+elixrs:AddDropdown({  -- Choose Elixir
 	Name = "🧪 Choose Elixir",
 	Default = "",
 	Options = {"Farm (Coin & XP)","Coin","XP","Lucky","Sea"},
@@ -96,7 +106,7 @@ elixrs:AddDropdown({
 
 
 -- FISHING
-fishing:AddToggle({
+fishing:AddToggle({  -- Auto-Sell Fish
 	Name = "🐟 Auto-Sell Fish",
 	Default = false,
 	Callback = function(Value)
@@ -107,7 +117,7 @@ fishing:AddToggle({
 
 
 -- AUTOBUY
-autobuy:AddToggle({
+autobuy:AddToggle({  -- Buy Auburn Shop
 	Name = "💰 Buy Auburn Shop",
 	Default = false,
 	Callback = function(Value)
@@ -115,7 +125,7 @@ autobuy:AddToggle({
         if ABAuburnShop == true then warn("[Debug] ✅ Enabled Auto-Buy Auburn Shop") end
 	end    
 })
-autobuy:AddToggle({
+autobuy:AddToggle({  -- Buy Magic Shop
 	Name = "💰 Buy Magic Shop",
 	Default = false,
 	Callback = function(Value)
@@ -123,7 +133,7 @@ autobuy:AddToggle({
         if ABMagicShop == true then warn("[Debug] ✅ Enabled Auto-Buy Magic Shop") end
     end
 })
-autobuy:AddToggle({
+autobuy:AddToggle({  -- Buy Gem Trader
 	Name = "💎 Buy Gem Trader",
 	Default = false,
 	Callback = function(Value)
@@ -131,7 +141,7 @@ autobuy:AddToggle({
         if ABGemTrader == true then warn("[Debug] ✅ Enabled Auto-Buy Gem Trader") end
 	end    
 })
-autobuy:AddToggle({
+autobuy:AddToggle({  -- Buy Blackmarket
 	Name = "💎 Buy Blackmarket",
 	Default = false,
 	Callback = function(Value)
@@ -140,8 +150,9 @@ autobuy:AddToggle({
     end
 })
 
+
 -- SHRINES
-autoshrines:AddToggle({
+autoshrines:AddToggle({  -- Auto-Collect Shrines
 	Name = "⚱️ Auto-Collect Shrines",
 	Default = false,
 	Callback = function(Value)
@@ -150,68 +161,70 @@ autoshrines:AddToggle({
 	end
 })
 
+
 -- QUESTS
-quest:AddToggle({
-	Name = "📜 Auto-Claim Bruh Bounty Quest",
+quest:AddToggle({  -- Auto-Claim All Quests
+	Name = "📜 Auto-Claim All Quests",
 	Default = false,
 	Callback = function(Value)
-        AClaimBquest = Value
-        if AClaimBquest == true then warn("[Debug] ✅ Enabled Auto-Claim Bruh Bounty Quest") end
+        AClaimQuest = Value
+        if AClaimQuest == true then warn("[Debug] ✅ Enabled Auto-Claim All Quests") end
 	end    
 })
 
 
 -- MISC
 misc:AddLabel("AntiAFK is enabled by default")
-misc:AddButton({
+misc:AddButton({  -- Redeem All Codes
 	Name = "🏷️ Redeem All Codes",
 	Callback = function()
         for _, code in pairs(codes) do
             rstorage.Function:InvokeServer("RedeemCode", code)
-            warn("[Debug] ✅ Redeemed " .. code .. " code")
-            wait(0.5)
-        end    
+            wait(.5)
+        end
+        warn("[Debug] ✅ Redeemed all codes")
   	end    
 })
-misc:AddButton({
+misc:AddButton({  -- Destroy UI
 	Name = "❌ Destroy UI",
 	Callback = function()
         OrionLib:Destroy()
   	end    
 })
 
-while task.wait() do
+
+-- LOGIC
+while task.wait() do  -- Toggles Logic
     if AUElixir == true then
         local CoinDur = sui.Buffs.Treasure.Button.Time.Text
         local XPDur = sui.Buffs.Experienced.Button.Time.Text
         local LuckyDur = sui.Buffs["Feeling Lucky"].Button.Time.Text
         local SeaDur = sui.Buffs["Ocean's Blessing"].Button.Time.Text
-        -- print("[Debug] Coin Duration: " .. CoinDur .. ", XP Duration: " .. XPDur .. ", Lucky Duration: " .. LuckyDur .. ", Sea Duration: " .. SeaDur)
         if chosenElx == "Farm (Coin & XP)" then
             if (CoinDur == "0s" or CoinDur == "11m 11s") and (XPDur == "0s" or XPDur == "11m 11s") then
                 rstorage.Event:FireServer("UsePowerup", "Coin Elixir")
                 rstorage.Event:FireServer("UsePowerup", "XP Elixir")
-                warn("[Debug] ✅ Used Coin and XP Elixir")
+                warn("[Debug] ✅ Used Coin and XP elixirs")
             end
         elseif chosenElx == "Coin" then
             if CoinDur == "0s" or CoinDur == "11m 11s" then
                 rstorage.Event:FireServer("UsePowerup", "Coin Elixir")
-                warn("[Debug] ✅ Used Coin Elixir")
+                warn("[Debug] ✅ Used Coin elixir")
             end
         elseif chosenElx == "XP" then
             if XPDur == "0s" or XPDur == "11m 11s" then
                 rstorage.Event:FireServer("UsePowerup", "XP Elixir")
-                warn("[Debug] ✅ Used XP Elixir")
+                warn("[Debug] ✅ Used XP elixir")
             end
         elseif chosenElx == "Lucky" then
             if LuckyDur == "0s" or LuckyDur == "11m 11s" then
                 rstorage.Event:FireServer("UsePowerup", "Lucky Elixir")
-                warn("[Debug] ✅ Used Lucky Elixir")
+                warn("[Debug] ✅ Used Lucky elixir")
             end
         elseif chosenElx == "Sea" then
             if SeaDur == "0s" or SeaDur == "11m 11s" then
                 rstorage.Event:FireServer("UsePowerup", "Sea Elixir")
-                warn("[Debug] ✅ Used Sea Elixir")
+                warn("[Debug] ✅ Used Sea elixir")
             end
         end
         wait(1)
@@ -226,13 +239,38 @@ while task.wait() do
             end
         end
     end
-    if AClaimBquest == true then
-        rstorage.Event:FireServer("FinishedQuestDialog", "bruh-bounty")
+    if AClaimQuest == true then
+        for _, questFolder in pairs(quests) do
+            if questFolder.Name == "Template" then
+                local tasks = questFolder.Tasks:GetChildren()
+                local questnames = {"bruh-bounty", "sailor-treasure-hunt"}
+
+                for _, taskFolder in pairs(tasks) do
+                    if taskFolder.Name == "Template1" then
+                        local titleText = taskFolder.Title.Text
+                        if string.find(titleText, "Return") then
+                            for _, quest in pairs(questnames) do
+                                rstorage.Event:FireServer("FinishedQuestDialog", quest)
+                                -- warn("[Debug] ✅ Completed a " .. quest .. " quest")
+                                wait(.1)
+                            end
+
+                            for i = 1, 30 do
+                                local omackaQ = "omacka-" .. i
+                                rstorage.Event:FireServer("FinishedQuestDialog", omackaQ)
+                                -- warn("[Debug] ✅ Tried to complete " .. omackaQ .. " quest")
+                                wait(.1)
+                            end
+                        end
+                    end
+                end
+            end
+        end
         wait(5)
     end
     if ASFish == true then
         rstorage.Event:FireServer("SellFish")
-        wait(0.1)
+        wait(.1)
     end
     if ABAuburnShop == true then
         for i = 1, 3 do
@@ -259,5 +297,6 @@ while task.wait() do
         wait(.05)
     end
 end
+
 
 OrionLib:Init()
