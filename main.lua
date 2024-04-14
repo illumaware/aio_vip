5db1f54ee16884845fb3c495fcc01b5d5a68d8fe
illumaware <66912298+illumaware@users.noreply.github.com>
2024-04-15 01:54:25 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,12 +5,13 @@ getgenv().aioExec = true
 local scripts = {
     [16510724413] = "PetCatchers",
     [5991163185]  = "SprayPaint",
-    [8884433153]  = "CollectAllPets"
+    [8884433153]  = "CollectAllPets",
+    [16524008257]  = "AnimeRNG"
 }
 
 local name = scripts[game.PlaceId]
 if name then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/illumaware/c/main/games/"..name..".lua"))()
+    loadstring(game:HttpGet("https://github.com/illumaware/c/blob/main/games/"..name..".lua?raw=true"))()
 else
     warn("[AIO] PlaceId "..game.PlaceId.." is not supported")
 end
